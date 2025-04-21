201453245a0c53cf21d169f8686aef82fd3eafaa
mindPazi <gitmind87@gmail.com>
2025-04-21 23:02:46 +0200

    minor fixes

--- src/main/java/com/github/mindpazi/flasksupportplugin/statusbar/VarTypeStartupActivity.java
+++ src/main/java/com/github/mindpazi/flasksupportplugin/statusbar/VarTypeStartupActivity.java
@@ -1,20 +1,16 @@
 package com.github.mindpazi.flasksupportplugin.statusbar;
 
 import com.github.mindpazi.flasksupportplugin.i18n.VarTypeBundle;
-import com.github.mindpazi.flasksupportplugin.statusbar.widget.VarTypeStatusBarWidget;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.EditorFactory;
-import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.openapi.wm.StatusBar;
-import com.intellij.openapi.wm.StatusBarWidget;
-import com.intellij.openapi.wm.WindowManager;
 import org.jetbrains.annotations.NotNull;
 
-public class VarTypeStartupActivity implements StartupActivity, DumbAware {
+public class VarTypeStartupActivity implements StartupActivity {
     private static final Logger LOG = Logger.getInstance(VarTypeStartupActivity.class);
+    private static final String startupRegisteredMsg = VarTypeBundle.message("log.startup.registered");
+    private static final String startupMessage = VarTypeBundle.message("log.startup.running");
 
     @Override
     public void runActivity(@NotNull Project project) {
@@ -23,7 +19,6 @@ public class VarTypeStartupActivity implements StartupActivity, DumbAware {
             return;
         }
 
-        String startupMessage = VarTypeBundle.message("log.startup.running", project.getName());
         LOG.info(startupMessage);
 
         try {
@@ -33,11 +28,9 @@ public class VarTypeStartupActivity implements StartupActivity, DumbAware {
                 return;
             }
 
-            cleanupExistingWidget(project);
-
             VarTypeListener listener = new VarTypeListener(project);
             EditorFactory.getInstance().getEventMulticaster().addCaretListener(listener, project);
-            String startupRegisteredMsg = VarTypeBundle.message("log.startup.registered");
+
             LOG.info(startupRegisteredMsg);
 
         } catch (Exception e) {
@@ -46,35 +39,4 @@ public class VarTypeStartupActivity implements StartupActivity, DumbAware {
         }
     }
 
-    private void cleanupExistingWidget(@NotNull Project project) {
-        if (project.isDisposed()) {
-            return;
-        }
-
-        ApplicationManager.getApplication().invokeLater(() -> {
-            try {
-                StatusBar statusBar = WindowManager.getInstance().getStatusBar(project);
-                if (statusBar != null) {
-                    StatusBarWidget existingWidget = statusBar.getWidget(VarTypeStatusBarWidget.ID);
-                    if (existingWidget != null) {
-                        LOG.info(VarTypeBundle.message("log.cleanup.existing.widget"));
-
-                        try {
-                            existingWidget.dispose();
-                        } catch (Exception e) {
-                            LOG.warn(VarTypeBundle.message("log.error.disposing.widget"), e);
-                        }
-
-                        try {
-                            statusBar.removeWidget(VarTypeStatusBarWidget.ID);
-                        } catch (Exception e) {
-                            LOG.warn(VarTypeBundle.message("log.error.removing.widget"), e);
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                LOG.error(VarTypeBundle.message("log.error.cleanup.startup"), e);
-            }
-        });
-    }
 }
